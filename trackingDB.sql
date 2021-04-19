bf6163c172d1d6dc204f9d2d6f07f5c0f3df6dea
Clay Jones <cgjones0711@gmail.com>
2021-04-19 16:14:26 -0500

    first commit

---
+++ trackingDB.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS trackingDB;
+CREATE database trackingDB;
+
+USE trackingDB;
+
+CREATE TABLE departments (
+  id INT NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+  
+);
+CREATE TABLE roles (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL (10,4) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+  
+
+SELECT * FROM top5000;
+SELECT * FROM employee;
+
